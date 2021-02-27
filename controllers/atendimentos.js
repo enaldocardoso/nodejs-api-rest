c3d38e937b2a30362b32db19238658c507183dc6
enaldocardoso <enaldocardoso@gmail.com>
2021-02-26 22:31:03 -0300

    projeto concluído

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,31 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
 
-    app.get('/atendimentos', (req,res) => res.send('Você está na roda de atendimentos GET') )
+    app.get('/atendimentos', (req,res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorId(id,res)
+    })
     
     app.post('/atendimentos', (req,res) => {
-        console.log(req.body)
-        res.send('Você está na roda de atendimentos POST')
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento, res)
     
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.deleta(id, res)
     })
 
 }
